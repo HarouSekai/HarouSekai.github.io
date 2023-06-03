0f95bc2fb5b0a8527be79a993b10f9a7895c709e
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-03 21:45:26 +0900

    prismの導入と読み込み

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-window.onload = function() {
+function main() {
+  const prism = document.createElement("script");
+  prism.src = "../javascript/prism.js";
+  document.head.appendChild(prism);
+
   const url = new URL(window.location.href);
   const params = url.searchParams;
   const tag = params.get("tag");
   const title = document.getElementById("title");
   title.innerHTML = tag;
-}
+};
+
+window.addEventListener('load', main);
