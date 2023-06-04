cbe487056d9d2e4b2aa2b92ea17afafefc83f8c2
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-04 21:09:07 +0900

    React

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
-function main() {
+const importModules = function() {
   const prism = document.createElement("script");
   prism.src = "../javascript/prism.js";
   document.head.appendChild(prism);
+}
 
+const menuBtn = function() {
+  const menuBtn = document.getElementById("menu-btn");
+  const menuItems = document.getElementById("menu-items");
+  const closeBtn = document.getElementById("close-btn");
+  menuBtn.addEventListener("click", function() {
+    this.setAttribute("style", "display:none;");
+    menuItems.setAttribute("style", "display:block;");
+    menuItems.addEventListener("click", function() {
+      this.setAttribute("style", "display:none;");
+      menuBtn.setAttribute("style", "display:flex;");
+    });
+  });
+}
+
+function main() {
+  importModules();
+  menuBtn();
   const url = new URL(window.location.href);
   const params = url.searchParams;
   const tag = params.get("tag");
