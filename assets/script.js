44819ed307aa5ca0d13604d85035496a3d62ec27
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-06-05 13:54:58 +0900

    即時化

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,27 @@
-const importModules = function() {
-  const prism = document.createElement("script");
-  prism.src = "../javascript/prism.js";
-  document.head.appendChild(prism);
-}
-
-const menuBtn = function() {
-  const menuBtn = document.getElementById("menu-btn");
-  const menuItems = document.getElementById("menu-items");
-  const closeBtn = document.getElementById("close-btn");
-  menuBtn.addEventListener("click", function() {
-    this.setAttribute("style", "display:none;");
-    menuItems.setAttribute("style", "display:block;");
-    menuItems.addEventListener("click", function() {
+window.addEventListener('load', function() {
+  (function() {
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+    const tag = params.get("tag");
+    const title = document.getElementById("title");
+    title.innerHTML = tag;
+  })();
+  (function() {
+    const prism = document.createElement("script");
+    prism.src = "../javascript/prism.js";
+    document.head.appendChild(prism);
+  })();
+  (function() {
+    const menuBtn = document.getElementById("menu-btn");
+    const menuItems = document.getElementById("menu-items");
+    const closeBtn = document.getElementById("close-btn");
+    menuBtn.addEventListener("click", function() {
       this.setAttribute("style", "display:none;");
-      menuBtn.setAttribute("style", "display:flex;");
+      menuItems.setAttribute("style", "display:block;");
+      menuItems.addEventListener("click", function() {
+        this.setAttribute("style", "display:none;");
+        menuBtn.setAttribute("style", "display:flex;");
+      });
     });
-  });
-}
-
-function main() {
-  importModules();
-  menuBtn();
-  const url = new URL(window.location.href);
-  const params = url.searchParams;
-  const tag = params.get("tag");
-  const title = document.getElementById("title");
-  title.innerHTML = tag;
-};
-
-window.addEventListener('load', main);
+  })();
+});
