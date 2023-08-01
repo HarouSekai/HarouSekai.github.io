2025a716afb2d3a857ffae59b2721676f0c0bc5e
koku503 <133859216+HarouSekai@users.noreply.github.com>
2023-08-02 00:06:43 +0900

    JavaScriptバックアップ

---
+++ javascript/script-backup.js
@@ -0,0 +1,32 @@
+window.addEventListener('load',()=>{
+  const d=document;
+  !function(){
+    const l=d.createElement('link');
+    l.href='../../assets/stylesheet.css';
+    l.rel='stylesheet';d.head.appendChild(l)
+  }(),
+  function(){
+    const h=d.querySelector('header .content');
+    h.setAttribute('id','header-content');
+    const m=d.querySelector('.main .content');
+    const t=new URL(window.location.href).searchParams;
+    const c=t.get('dir');
+    let e=t.get('tag');
+    const n=d.getElementById('title');
+    d.title=n.innerHTML=e;
+    const b=`<div id='pan'><div><a href=../../assets/index.html?tag=技術記録 style='text-decoration:underline;color:#48f'>HOME</a>&nbsp;&gt;&nbsp;</div><div>${c}&nbsp;&gt;&nbsp;</div><div>${e}</div></div>`;
+    if (e !== '技術記録') {
+      m.insertAdjacentHTML('afterbegin',b)
+    }
+  }(),
+  function(){
+      const t=d.createElement('script');
+      t.src='../../javascript/prism.js',
+      d.head.appendChild(t)
+  }(),
+  function(){
+    const t=d.createElement('script');
+    t.src='../../javascript/menu.js',
+    d.head.appendChild(t)
+  }()
+});
